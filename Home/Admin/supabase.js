d5544013d36af27a5f0b44a82fe3b63c55c620e2
unknown <your-email@example.com>
2026-05-09 01:48:12 +0800

    Fix Supabase connection

--- Home/Admin/supabase.js
+++ Home/Admin/supabase.js
@@ -1,11 +1,10 @@
 const SUPABASE_URL =
-  "YOUR_API_URL";
+  "sb_publishable_yWOmkaQzsh7sInJPhDOFWw_tyjALAuP";
 
 const SUPABASE_KEY =
-  "YOUR_PUBLISHABLE_KEY";
+  "sb_secret_DTrj2ilDnomG3lUBOqPZGQ_2fJNsjxb";
 
-const supabaseClient =
-  supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-  );
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
