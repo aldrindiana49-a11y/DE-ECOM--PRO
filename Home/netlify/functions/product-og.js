165318c8406f1d7faf2ad91c303453420afea19a
unknown <your-email@example.com>
2026-06-01 21:04:42 +0800

    Final Facebook OG fix

--- Home/netlify/functions/product-og.js
+++ Home/netlify/functions/product-og.js
@@ -62,15 +62,13 @@ exports.handler = async (event) => {
         <meta property="og:title" content="${title}" />
         <meta property="og:description" content="${description}" />
         <meta property="og:image" content="${image}" />
-        <meta property="og:url" content="${url}" />
+        <meta property="og:url" content="https://drinelectronicsph.com/.netlify/functions/product-og?id=${id}" />
         <meta property="og:type" content="website" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="${title}" />
         <meta name="twitter:description" content="${description}" />
         <meta name="twitter:image" content="${image}" />
-
-        <meta http-equiv="refresh" content="2;url=${url}">
 
       </head>
       <body>
